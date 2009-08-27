948cc3ea99e59f0dcd4ae4a470cb5fb9ff0e28d1
Matthew Toseland <toad@amphibian.dyndns.org>
2009-08-27 21:13:23 +0100

    2: works with current code, new build structure so we don't need the jars

--- src/plugins/Freereader/Version.java
+++ src/plugins/Freereader/Version.java
@@ -13,7 +13,7 @@ public class Version {
 	 * a major change, a significant bugfix etc. These numbers are used in auto-update 
 	 * etc, at a minimum any build inserted into auto-update should have a unique 
 	 * version. */
-	public static long version = 1;
+	public static long version = 2;
 	
 	static String getSvnRevision() {
 		return svnRevision;
