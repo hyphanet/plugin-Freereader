be8cf273355335b36e19f40701681e1302e86092
Florent Daigniere <nextgens@freenetproject.org>
2016-06-03 11:04:27 +0200

    Release v6

--- src/plugins/Freereader/Version.java
+++ src/plugins/Freereader/Version.java
@@ -13,7 +13,7 @@ public class Version {
 	 * a major change, a significant bugfix etc. These numbers are used in auto-update 
 	 * etc, at a minimum any build inserted into auto-update should have a unique 
 	 * version. */
-	public static long version = 4;
+	public static long version = 6;
 	
 	static String getGitRevision() {
 		return gitRevision;
